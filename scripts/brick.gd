8dca977688f0525353b9c2ba02a6c1b3bcb1d4d4
lima6-tech <lima6@kgv.hk>
2026-02-28 13:01:07 +0800

    brick

--- scripts/brick.gd
+++ scripts/brick.gd
@@ -1,7 +1,4 @@
 extends Area2D
-<<<<<<< HEAD
-=======
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +8,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
->>>>>>> 1c27375d09bdbe9ddc6cc7207e4e6284d8f53041
