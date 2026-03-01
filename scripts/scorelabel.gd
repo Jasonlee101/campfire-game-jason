e86a5956a6c81b5a1a1bd1d469f0e813e62aa2b0
Jason <leej70@kgv.hk>
2026-03-01 10:26:04 +0800

    boom boom

--- scripts/scorelabel.gd
+++ scripts/scorelabel.gd
@@ -3,6 +3,3 @@ extends Label
 func _process(_delta: float) -> void:
 	# 1. Update the text
 	text = "Score: %d" % Stats.score
-	
-	# 2. DEBUG: This will flood your output, but it proves the script is running!
-	print("Label is trying to show: ", text)
