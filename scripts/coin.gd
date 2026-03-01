a9adcdb8bd58cadc9596e439792c6839a1a6c7a6
Jason <leej70@kgv.hk>
2026-03-01 12:42:55 +0800

    music and sfx

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,7 @@
 extends Area2D
 @onready var game_manager: Node = %GameManager
-
+@onready var animation_player = $AnimationPlayer
 func _on_body_entered(body):
 	Stats.score += 1
 	print("Your score is:", Stats.score)
-	queue_free()
+	animation_player.play("pickup")
