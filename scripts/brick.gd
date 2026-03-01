3fe523fb8e65d6097ac7364e5bae4b987c2786a7
lima6-tech <lima6@kgv.hk>
2026-03-01 10:26:27 +0800

    stuff

--- scripts/brick.gd
+++ scripts/brick.gd
@@ -6,6 +6,9 @@ extends StaticBody2D
 @export var interaction_range: float = 50.0
 @onready var player = get_tree().get_first_node_in_group('player')
 
+func _on_ready() -> void:
+	print("I'm a brick")
+
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	# Use the 'event' parameter for better mouse detection
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
