113c2b76d8d81b3e2d7475ae6659c31603abe7d8
lima6-tech <lima6@kgv.hk>
2026-03-13 22:22:40 +0800

    Updates

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,15 @@
 extends Area2D
+
 @onready var game_manager: Node = %GameManager
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite = $AnimatedSprite2D # Reference the sprite
 
 func _on_body_entered(body):
-	var death = body.dead
-	if not death: 
-		stats.score += 1
+	if not body.dead: 
+		stats.score += 1 
+		
+		if animated_sprite.sprite_frames.has_animation("sparkle"):
+			animated_sprite.play("sparkle")
+			print('playing')
+		
 		animation_player.play("pickup")
