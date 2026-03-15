15e16bdf8590f5df2583ee1ae381fc9286ef0d19
Jason <leej70@kgv.hk>
2026-03-15 10:48:13 +0800

    fixes

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,7 +23,7 @@ var empty_heart_rect = Rect2(16, 0 , 16,16)
 @onready var slash: AnimatedSprite2D = $Slash
 
 func _ready() -> void:
-	var settings =Global.get_difficulty_settings() 
+	var settings = Global.get_difficulty_settings() 
 	max_health = settings.hearts
 	current_health = max_health
 	
